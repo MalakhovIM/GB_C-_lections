bbc0bc8af9c1a49d61b6186ea218af79735d9a1c
IgorMalakhov <forvord_next@mail.ru>
2022-12-01 10:01:23 +0300

    progect 007 intro method

---
+++ progect007_IntroMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿int FindMax(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+int max = FindMax(FindMax(array[0], array[1], array[2]), FindMax(array[3], array[4], array[5]), FindMax(array[6], array[7], array[8])); 
+
+Console.WriteLine(max);
